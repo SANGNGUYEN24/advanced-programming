af78cf3581e372f82143b479d7ec3c7b3574f6b3
Sang Nguyen <sangnguyendinh24work@gmail.com>
2021-07-20 13:59:01 +0700

    update

--- .vscode/main.hs
+++ .vscode/main.hs
@@ -23,7 +23,31 @@ isEven n = mod n 2 == 0
 
 coolRating :: Int -> Int -> String
 coolRating  param1 param2
-    | 
+    | res <= 10 = "Cool"
+    | res <= 20 = "Quite cool"
+    | res <= 40 = "Super cool"
+    | otherwise = "Super stars"
+    where res = param1 + param2
+
+getList :: [Int] -> String
+getList [] = "Your list is empty"
+getList (x:[]) = "Your list starts with " ++ show x
+getList (x:y:[]) = "Your list with " ++ show x ++ " and contains " ++ show y
+getList (x:y) = "Your list starts with " ++ show x ++ " and the rest are " ++ show y
+
+reversedString = []
+getFirstItem :: String -> String
+getFirstItem [] = "Empty list"
+getFirstItem all@(x:xs) = all ++ " ends with " ++ [x]
+
+times4 :: Int -> Int
+times4 x = x * 4
+
+listTimes4 = map times4 [1,3,4]
+
+
+
+
 
 
 
